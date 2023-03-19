1a610d4192a5988667c80bb3263d0883282285ff
lauta <lautanaj@hotmail.com>
2023-03-19 16:48:19 -0300

    commiteo

--- ES.js
+++ ES.js
@@ -1,10 +1,20 @@
+const fs = require("fs");
+
 // Exercise ProductManager.
 
 class ProductManager {
+  path; // La ruta del archivo de productos a generar.
   products; // Products.
 
-  constructor() {
+  constructor(path) {
+    this.path = path;
     this.products = [];
+    // Si no existe el archivo de productos, crearlo.
+    if (!fs.existsSync(this.path))
+      fs.writeFile(this.path, this.products, (err) => {
+        if (err) throw err;
+        else console.log(`Archivo ${this.path} creado.`);
+      });
   }
 
   // Add the product by parameter
@@ -14,77 +24,47 @@ class ProductManager {
   }
 
   // Return all products.
-  getProducts() {
+  async getProducts() {
+    await fs.readFile(this.path, "utf8", (err, data) => {
+      // Transformo el contenido JSON a variable en memoria.
+      let data_archive = JSON.parse(data);
+      // Agregamos al array parseado con los datos cargados del archivo a this.products.
+      this.products = data_archive;
+    });
     return this.products;
   }
 
   getProductsById(id) {
+    this.getProducts();
     let Found = this.products.find((product) => product.id === id);
-    return Found ? Found : console.error("Not Found.");
+    if (!Found) {
+      console.log("Product Not Found");
+      return null;
+    }
+    return Found;
+  }
+
+  updateProduct(id, new_data) {
+    this.getProducts();
+    let updateProd= this.getProductsById(id);
+    if (updateProd != null) {
+      // Obtiene las keys del objeto a actualizar, lo almaceno a keys array.
+      const keys = Object.keys(new_data);
+      keys.forEach((key) => {
+        producto_a_actualizar[key] = new_data[key];
+      });
+    }
   }
 }
 
-const products = [
-  {
-    title: "Placa De Video ASUS Rog Strix 3070ti",
-    description: "Placa De Video",
-    price: 201000, 
-    code: "1822030398D",
-    thumbnail:
-    "https://http2.mlstatic.com/D_NQ_NP_644879-MLA48656467017_122021-O.webp",
-    stock: 15,
-  },
-  {
-    title: "Placa De Video MSI rtx 3050",
-    description: "Placa De Video",
-    code: "1822030398W",
-    thumbnail:
-      "https://www.fullh4rd.com.ar/img/productos/Pics_Prod/video-geforce-rtx-3050-8gb-msi-gaming-x-0.jpg",
-    price: 99000,
-    stock: 7,
-  },
-  {
-    title: "Placa De Video Nvidia Asus TUF RTX 3080",
-      description: "Placa De Video",
-    code: "1822030398Q",
-    thumbnail:
-      "https://http2.mlstatic.com/D_NQ_NP_781239-MLA48656710911_122021-O.webp",
-    price: 300000,
-    stock: 20,
-  },
-  {
-    title: "Procesador AMD Ryzen™ 7 5700G",
-    description: "Procesador",
-    code: "1822030398F",
-    thumbnail:
-      "https://http2.mlstatic.com/D_NQ_NP_940934-MLU47593217192_092021-O.webp",
-    price: 65000,
-    stock: 6,
-  },
-  {
-    title: "Procesador Intel Core i7-10700",
-    description: "Procesador",
-    code: "1822030398Y",
-    thumbnail:
-      "https://http2.mlstatic.com/D_NQ_NP_729444-MLA42903822558_072020-O.webp",
-    price: 70000,
-    stock: 6,
-  },
-  {
-    title: "Motherboard Asus Rog Strix B550-f",
-    description: "Motherboard",
-    code: "1822030398O",
-    thumbnail:
-      "https://mexx-img-2019.s3.amazonaws.com/Motherboard-Am4-Asus-Rog-Strix-B550-F-GAMING-WIFI-II_43255_1.jpeg",
-    price: 54000,
-    stock: 3,
-  },
-];
-
-let productManager = new ProductManager();
-products.forEach((product) => productManager.addProduct(product));
+let productManager = new ProductManager("./productos.json");
 console.log("===== Products =====");
-let allProducts = productManager.getProducts();
-console.log(allProducts);
-let findProduct = productManager.getProductsById(1);
-console.log(findProduct);
+productManager.addProduct({
+  title: "Placa De Video Nvidia Asus TUF RTX 3080",
+  description: "Placa De Video",
+  code: "1822030398Q",
+  thumbnail:
+    "https://http2.mlstatic.com/D_NQ_NP_781239-MLA48656710911_122021-O.webp",
+  price: 300000,
+  stock: 20,
+});
