22284882a02719800f499cc41ed53585c07d198e
lauta <lautanaj@hotmail.com>
2023-04-22 18:44:23 -0300

    multer

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import express from "express";
 import cartEndPoint from "./routes/CartEndPoint.js";
 import ProductViews from "./routes/ProductViews.js";
 import ProductEndPoint from "./routes/ProductEndPoint.js";
+import userRouter from "./routes/UserRouter.js";
 import handlebars from "express-handlebars";
 
 const port = 8081;
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 /* Starting Handlebars */
 app.engine("handlebars", handlebars.engine());
@@ -20,6 +22,9 @@ app.use("/public", express.static("public"));
 app.use("/api/products", ProductEndPoint);
 app.use("/api/carts", cartEndPoint);
 app.use("/home", ProductViews);
+/* Multer Start */
+app.use("/api/users", userRouter);
+/* Multer End*/
 
 const connectionInstance = app.listen(port, () => {
   console.log(`Server is running on port 8081 ${port}`);
