0239956c936ffdccf62af19716c9a35c96a58a68
lauta <lautanaj@hotmail.com>
2023-05-15 13:56:32 -0300

    Cookies

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cartRouter from "./routes/CartRouter.js";
 import ProductViews from "./routes/ProductViews.js";
 import ProductsRouter from "./routes/ProductsRouter.js";
+import cookieRouter from "./routes/CookieRouter.js";
 import handlebars from "express-handlebars";
 
 
@@ -29,6 +30,9 @@ app.use("/home", ProductViews);
 /* Multer Start */
 // app.use("/api/multer", MulterRouter);
 /* Multer End*/
+/* CookieParser */
+app.use("/api/cookies", cookieRouter);
+/* CookieParser */
 
 const connectionInstance = app.listen(port, () => {
   console.log(`Server is running on port 8081 ${port}`);
