1aa3b34dc35c7d3260628df3ee1e112ff9d5ff03
LautaroP <lautaro@superiordigital.design>
2023-06-18 22:25:16 -0300

    refactor Express & WebSocket

--- Index.js
+++ Index.js
@@ -1,30 +1,8 @@
-import { Server } from "socket.io";
-import ProductManager from "./src/data/Dao/ProductManager.js";
-import connectionInstance from "./src/app.js";
+import AppExpress from "./src/appFactory/AppExpress.js";
 
-const ProductsWebSocket = new Server(connectionInstance);
-const productManager = new ProductManager();
+const app = new AppExpress();
+app.init()
+app.build()
+app.listen()
 
-ProductsWebSocket.on("connection", (client) => {
-  client.on("on_connect", (data) => {
-    console.log(data);
-  });
-  client.on("create_product", (data) => {
-    console.log(data);
-    let newProduct = productManager.addProduct(data);
-    client.emit("create_ok", {
-      code: "OK",
-      product: newProduct,
-    });
-  });
-  client.on("delete_product", (data) => {
-    console.log(data.product)
-    let deleted = productManager.deleteProduct(parseInt(data.product));
-    client.emit("delete_ok", deleted);
-  });
-  client.on("disconnect", () => {
-    console.log("Client disconnected");
-  });
-});
-  
-export default ProductsWebSocket;
+export default app;
